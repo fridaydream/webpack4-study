32fe4599a238f3bdaba7f1e28139e3fdd23b77e6
609457773@qq.com <609457773@qq.com>
2018-10-25 09:53:07 +0800

    f cm1

---
+++ spa-webpack/config/webpack.development.js
@@ -0,0 +1,17 @@
+module.exports = {
+  output: {
+    filename: 'scripts/[name].bundles.js'
+  },
+  devServer: {
+    port: 3000,
+    hot: true,
+    before(app) {
+      app.get('/api/test', (req, res) => {
+        res.json({
+          code: 200,
+          message: 'hello world'
+        })
+      })
+    }
+  }
+}
--- spa-webpack/webpack.config.js
+++ spa-webpack/webpack.config.js
@@ -2,8 +2,22 @@ const WebpackDeepScopeAnalysisPlugin = require('webpack-deep-scope-plugin').defa
 
 const PurifyCSSPlugin = require('purifycss-webpack')
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const argv = require('yargs-parser')(process.argv.slice(2))
+const merge = require('webpack-merge')
+const _mode = argv.mode || 'development'
+const _modeflag = _mode === 'production'
+const _mergeConig = require(`./config/webpack.${_mode}.js`)
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const {
+  join
+} = require('path')
+
 const glob = require('glob')
 const path = require('path')
+const setIterm2Badge = require('set-iterm2-badge')
+setIterm2Badge('开发环境')
 const config = {
   module: {
     rules: [
@@ -18,17 +32,22 @@ const config = {
     ]
   },
   plugins: [
-    new WebpackDeepScopeAnalysisPlugin(),
+    // new WebpackDeepScopeAnalysisPlugin(),
     new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
       // both options are optional
-      filename: "[name].css",
-      chunkFilename: "[id].css"
+      filename: _modeflag ? "styles/[name].[hash:5].css" : "styles/[name].css",
+      chunkFilename: _modeflag ? "styles/[id].[hash:5].css" : "styles/[id].css"
     }),
     new PurifyCSSPlugin({
-      paths: glob.sync(path.join(__dirname, './dist/*.html'))
-    })
+      paths: glob.sync(path.join(__dirname, './src/*.html'))
+    }),
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/index.html'
+    }),
   ]
 }
 
-module.exports = config
+module.exports = merge(config, _mergeConig)
